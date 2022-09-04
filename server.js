f9691cc475678d6f470d8cada774e5feee0dde75
kristafrancis721 <kristafrancis08@yahoo.com>
2022-09-04 13:33:21 -0400

    import connection to sequelized in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+//importing connection to Sequelize from config/connection.js
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,9 +9,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// turn on routes
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// turn on connection to db and server
+//Sequelize taking the models and connecting them to associated database tables
+//if it doesn't find a table it will create it for you
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
