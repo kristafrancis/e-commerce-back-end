d708ce5490d2ac30d1e4e1f8df85f9d036583b5e
kristafrancis721 <kristafrancis08@yahoo.com>
2022-09-07 15:14:21 -0400

    fixed typos

--- models/Product.js
+++ models/Product.js
@@ -42,13 +42,6 @@ Product.init(
         key: "id",
       },
     },
-    tag_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "tag",
-        key: "id",
-      },
-    },
   },
   {
     sequelize,
